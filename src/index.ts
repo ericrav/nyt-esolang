7b10b9f3fde739890ee0dad6e2c7cb4d8aeb1062
Eric Rabinowitz <eric.rabinowitz@nytimes.com>
2022-08-04 12:53:45 -0400

    hello world!

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
-import { readFileSync } from 'fs';
+import { readdirSync, readFileSync } from 'fs';
 import { parse, TextNode, HTMLElement } from 'node-html-parser';
 import { Tokenizer } from './tokenizer';
 import { Parser } from './parser';
 import { Interpreter } from './interpreter';
+import path from 'path';
 
 function main() {
-  const filename = process.argv[2];
-  const file = readFileSync(filename);
-  const html = parse(file.toString());
-  const body = html.querySelector('body')!;
-  const tokenizer = new Tokenizer(body);
-  const tokens = tokenizer.tokenize();
-  const parser = new Parser(tokens);
-  const ast = parser.parse();
-  const interpreter = new Interpreter(ast, {
+  const folder = process.argv[2];
+  const files = readdirSync(folder);
+  const program = files.reduce((dict, filename) => {
+    const file = readFileSync(path.join(folder, filename));
+    const html = parse(file.toString());
+    const body = html.querySelector('body')!;
+    const tokenizer = new Tokenizer(body);
+    const tokens = tokenizer.tokenize();
+    const parser = new Parser(tokens);
+    const ast = parser.parse();
+    dict[filename] = ast;
+    return dict;
+  }, {});
+  const interpreter = new Interpreter(program['main.html'], {
     input: () => '',
     output: (str) => process.stdout.write(str),
-  });
+  }, program);
   interpreter.evaluate();
   process.stdout.write('\n');
 }
